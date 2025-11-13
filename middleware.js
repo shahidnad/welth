2b56c3b2b79e8955a23c568841df70a6e86822b4
shahid nadaf <158706478+shahidnad@users.noreply.github.com>
2025-11-13 16:15:28 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,12 +1,10 @@
 import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
   "/transaction(.*)",
 ]);
-
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
   rules: [
@@ -19,22 +17,17 @@ const aj = arcjet({
     }),
   ],
 });
-
 const clerk = clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth();
-
   if (!userId && isProtectedRoute(req)) {
     return redirectToSignIn();
   }
 });
-
 export default createMiddleware(aj, clerk);
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
